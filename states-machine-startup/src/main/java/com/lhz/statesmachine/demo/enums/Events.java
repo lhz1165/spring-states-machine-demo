a87398bc0b552c5229220f35ad07688bd29d2f2b
laihz <2190732276@qq.com>
2023-11-25 11:03:23 +0800

    init

---
+++ states-machine-startup/src/main/java/com/lhz/statesmachine/demo/enums/Events.java
@@ -0,0 +1,9 @@
+package com.lhz.statesmachine.demo.enums;
+
+/**
+ * @author laihz
+ * @date 2023/11/23 15:25
+ */
+public enum Events {
+    E1, E2
+}
---
+++ states-machine-startup/src/main/java/com/lhz/statesmachine/demo/enums/States.java
@@ -0,0 +1,9 @@
+package com.lhz.statesmachine.demo.enums;
+
+/**
+ * @author laihz
+ * @date 2023/11/23 15:25
+ */
+public enum States {
+    SI, S1, S2,SF
+}
